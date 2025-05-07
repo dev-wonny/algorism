af35c0392b14f6d578ee8f3f97610fa88eb196ca
“devwonny” <161826579+dev-wonny@users.noreply.github.com>
2025-05-07 21:36:03 +0900

    [Add] binary search

---
+++ src/main/_202505/binarysearch/SearchinRotatedSortedArray2.java
@@ -0,0 +1,15 @@
+package main._202505.binarysearch;
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+public class SearchinRotatedSortedArray2 {
+    public static void main(String[] args) {
+        SearchinRotatedSortedArray2 o = new SearchinRotatedSortedArray2();
+        System.out.println(o.search(new int[] {2, 5, 6, 0, 0, 1, 2}, 0));//t
+        System.out.println(o.search(new int[] {2, 5, 6, 0, 0, 1, 2}, 3));//f
+    }
+
+    public boolean search(int[] nums, int target) {
+
+        return false;
+    }
+}
